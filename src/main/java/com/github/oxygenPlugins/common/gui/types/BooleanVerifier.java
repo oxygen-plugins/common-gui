b9c79554f89e7340ec61da212ebb1425518dffd5
Nico Kutscherauer <kutscherauer@schematron-quickfix.com>
2017-10-02 15:49:37 +0200

    Removed unused BooleanPanels

--- src/main/java/com/github/oxygenPlugins/common/gui/types/BooleanVerifier.java
+++ src/main/java/com/github/oxygenPlugins/common/gui/types/BooleanVerifier.java
@@ -1,34 +1,7 @@
 package com.github.oxygenPlugins.common.gui.types;
 
-import java.awt.Container;
-
-import javax.swing.JFormattedTextField;
-
-import com.github.oxygenPlugins.common.gui.types.panels.BooleanPanel;
-import com.github.oxygenPlugins.common.gui.types.panels.BooleanPanel2;
-import com.github.oxygenPlugins.common.gui.types.panels.StringPanel;
-
-public class BooleanVerifier extends ValidCharVerifier {
+public class BooleanVerifier extends MultiChoiceVerifier {
 	public BooleanVerifier() {
-		this("");
-	}
-	public BooleanVerifier(String invalidStrings){
-		this(invalidStrings, "");
-	}
-	public BooleanVerifier(String invalidStrings, String invalidStarts){
-		super(invalidStrings, invalidStarts, false);
-	}
-	
-	@Override
-	public void setVerifier(LabelField field, Container owner) {
-		BooleanPanel2 sp = new BooleanPanel2(field, owner);
-//		field.removeFocusListener(sp);
-		field.addMouseListener(sp);
-//		field.removeFocusListener(sp);
-//		field.addFocusListener(sp);
-	}
-	@Override
-	public _Verifier getNewInstance() {
-		return new BooleanVerifier();
+		super(new String[]{"true", "false"},  false);
 	}
 }
